e659cfcc096c9eb54f1ab7902d94d3c2a946b345
Abhishek <abhibhovar@gmail.com>
2021-08-26 10:19:02 +0530

    Modified

--- LINQ/EntityFrame/EntityFrame/Program.cs
+++ LINQ/EntityFrame/EntityFrame/Program.cs
@@ -7,10 +7,12 @@ using System.Threading.Tasks;
 namespace EntityFrame
 {
     // EntityFrameWork works on top of ADO.NET
+    //Steps to use EFW
     // Proj->Add new item->Template->Data->Ado.net->Ef Designer Db->Connection
     // Connection -> new -> sever name -> db name -> Connect
     // Connection -> exist -> Connect
-    // Connect -> EF Ver -> Check Tables we want 
+    // Connect -> EF Ver -> Check Tables we want
+    // EDM(Entity Data Model) .edmx will be Constructed by the EFW 
 
     class Program
     {

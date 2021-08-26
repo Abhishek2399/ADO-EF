297abca389838d72fbd4a5b0c41d36d131774db0
Abhishek <abhibhovar@gmail.com>
2021-08-26 10:10:27 +0530

    Mods

--- LINQ/EntityFrame/EntityFrame/Program.cs
+++ LINQ/EntityFrame/EntityFrame/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; // for LINQ Queries
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityFrame
 {
     // EntityFrameWork works on top of ADO.NET
-
     class Program
     {
         static void Main(string[] args)

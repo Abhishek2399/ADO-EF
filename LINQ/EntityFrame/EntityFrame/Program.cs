9b6a995c09d155962d7c5093d253f1238d26a9e9
Abhishek <abhibhovar@gmail.com>
2021-08-26 10:17:38 +0530

    Modified

--- LINQ/EntityFrame/EntityFrame/Program.cs
+++ LINQ/EntityFrame/EntityFrame/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace EntityFrame
 {
     // EntityFrameWork works on top of ADO.NET
+    // Proj->Add new item->Template->Data->Ado.net->Ef Designer Db->Connection
+    // Connection -> new -> sever name -> db name -> Connect
+    // Connection -> exist -> Connect
+    // Connect -> EF Ver -> Check Tables we want 
+
     class Program
     {
         static void Main(string[] args)

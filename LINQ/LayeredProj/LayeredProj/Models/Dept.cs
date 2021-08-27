c5ee8341399efdaba1043670d590a58dea114c81
Abhishek <abhibhovar@gmail.com>
2021-08-27 09:27:26 +0530

    Layered Proj EFCore

---
+++ LINQ/LayeredProj/LayeredProj/Models/Dept.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DAL.Models
+{
+    public partial class Dept
+    {
+        public int Did { get; set; }
+        public string Name { get; set; }
+    }
+}
